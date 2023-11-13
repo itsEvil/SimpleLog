ccf07db2c4474633e19c2d213989d10b1dbb333c
itsEvil <56killer65@gmail.com>
2023-11-13 20:29:01 +0000

    Add project files.

---
+++ SimpleLog/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleLog
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            
+            SLog.Debug("debug");
+            SLog.Trace("trace");
+            SLog.Info("info");
+            SLog.Error("error");
+            SLog.Fatal(new Exception("fatal"), false); //This will spam your logs directory 
+
+            SLog.Info($"SLog::Config");
+            SLog.Info($"MinLevel::{SLogConfig.MinLevel}");
+            SLog.Info($"SleepTime::{SLogConfig.SleepTime}");
+            SLog.Info($"ExportErrors::{SLogConfig.ExportErrors}");
+
+            Console.ReadLine();
+        }
+    }
+}
